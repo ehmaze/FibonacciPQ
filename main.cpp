f8825e62b52717aa17668f0d3f619046399789d8
emaze <emaze@umich.edu>
2020-07-09 21:28:45 -0400

    updated test

--- main.cpp
+++ main.cpp
@@ -1,40 +1,31 @@
 #include <iostream>
 #include <cassert>
+#include <queue>
+#include <cstdint>
+#include <random>
+#include <vector>
+#include <functional>
 #include "FibonacciPQ.h"
 
 using namespace std;
 
 int main() {
-  FibonacciPQ<int> pq;
-  assert(pq.empty());
-  cout << "Pass\n";
-  pq.push(5);
-  assert(pq.top() == 5);
-  cout << "Pass\n";
-  pq.push(2);
-  pq.push(10);
-  assert(pq.top() == 2);
-  cout << "Pass\n";
-  assert(pq.size() == 3);
-  cout << "Pass\n";
-  pq.push(6);
-  pq.push(20);
-  pq.pop_top();
-  assert(pq.top() == 5);
-  cout << "Pass\n";
-  assert(pq.size() == 4);
-  cout << "Pass\n";
-  pq.pop_top();
-  assert(pq.top() == 6);
-  cout << "Pass\n";
-  pq.push(2);
-  assert(pq.top() == 2);
-  cout << "Pass\n";
-  pq.pop_top();
-  assert(pq.top() == 6);
-  cout << "Pass\n";
-  pq.pop_top();
-  assert(pq.top() == 10);
-  cout << "Pass\n";
+  FibonacciPQ<unsigned int> test;
+  priority_queue<unsigned int, vector<unsigned int>, greater<unsigned int> > real;
+  random_device rand;
+  for (size_t i = 0; i < 100; i++) {
+    unsigned int rd = rand() % 200;
+    test.push(rd);
+    real.push(rd);
+    if (i > 0 && i % 5 == 0) {
+      test.pop_top();
+      real.pop();
+      unsigned int a = test.top(), b = real.top();
+      if (a != b) {
+        cout << "ERROR\n";
+      } // if different
+    } //pop, compare
+  } //for
+  cout << "PASS\n";
   return 0;
 }
